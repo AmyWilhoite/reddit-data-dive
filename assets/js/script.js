0ca26ba09c424c6a96dd3451da95e6487d45ab75
AmyWilhoite <amywilhoite@gmail.com>
2022-02-09 19:37:57 -0500

    connected to Wikipedia API and tested

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,38 @@ $("#searchBtn").on("click", function(event) {
     //get the textbox value
     if($("#userData").val() ==="") {
         console.log( "type in something");
+    } else { 
+        searchOnWikipedia ($("#userData").val());
     }
     
 })
+
+function searchOnWikipedia (term) {
+   /*
+
+*/
+
+var url = "https://en.wikipedia.org/w/api.php"; 
+
+var params = {
+    action: "query",
+    list: "search",
+    srsearch: term,
+    srlimit: 1,
+    srprop: "snippet",
+    format: "json"
+};
+
+url = url + "?origin=*";
+Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+fetch(url)
+    .then(function(response){return response.json();})
+    .then(function(response) {
+    console.log(response);
+    })
+    .catch(function(error){console.log(error);});
+}
 
 $("#recentsearches").on("change", function() {
     alert("select changed");
